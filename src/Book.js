bbb9b154599df61ab751051d671cdc7f21b3dd1d
AshleyLoreal Leee <53842786+alyrlee@users.noreply.github.com>
2020-04-30 08:19:36 -0400

    initial commit

---
+++ src/Book.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Book.css';
+
+https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters
+
+export default function Book( props ) {
+
+    const { book } = props;
+        
+    let author;
+    let title;
+    let thumbnailUrl;
+    let previewUrl;
+    let snippet;
+    let cost;
+
+    return (
+        <div className="book_container">
+            <a href={ previewUrl } target="blank">
+                <li className="book_li">
+                    <img src={ thumbnailUrl } className="book_image" alt={`The cover of the book titled ${title}`}></img>
+                    <div className="book_info">
+                        <h2 className="book_title">{ title }</h2>
+                        <h4 className="book_author">{ author }</h4>
+                        <div className="book_cost">{ cost }</div>
+                        <p className="book_snippet">{ snippet }</p>
+                    </div>
+                </li>
+            </a>
+        </div>
+    );
+} 
---
+++ src/FilterOptions.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './FilterOptions.css';
+
+class FilterOptions extends Component {
+  render() {
+    const { handlePrintType, handleBookType } = props;
+    return (
+        <div className="filter_bar_container">
+            <label>Print Type:</label>
+            <form 
+                action="" 
+                className="print_type_filter_form"
+                onChange={ e => handlePrintType( e.target.value ) }>
+                <select 
+                    name="print-type-filter">
+                    <option 
+                        value="all">
+                        All
+                    </option>
+                    <option 
+                        value="books">
+                        Only Books
+                    </option>
+                    <option 
+                        value="magazines">
+                        Only Magazines
+                    </option>
+                </select>
+            </form>
+            <label>Book Type:</label> 
+            <form 
+                action="" 
+                className="book_type_filter_form"
+                onChange={ e => handleBookType( e.target.value ) }>
+                <select 
+                    name="book-type-filter">
+                    <option 
+                        value="ebooks">
+                            All eBooks
+                    </option>
+                    <option 
+                        value="free-ebooks">
+                        Free eBooks
+                    </option>
+                    <option 
+                        value="paid-ebooks">
+                        Paid eBooks
+                        </option>
+                    <option 
+                        value="full">
+                        Fully Available eBooks
+                    </option>
+                    <option 
+                        value="partial">
+                        Partially Available eBooks
+                    </option>
+                </select>
+            </form> 
+        </div>
+    );
+}
+}
