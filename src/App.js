19c1ce649e39938aa7385d2c7137b15513593d58
AshleyLoreal Leee <53842786+alyrlee@users.noreply.github.com>
2020-04-30 21:35:28 -0400

    refactored app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './SearchBar';
-import FilterableList from './FilterableList';
-import Book from './Book'; 
-import Header from './';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-      filterOption: 'All'
-    };
-  }
-
   render() {
     return (
       <div className="App">
-        <SearchBar
-          searchTerm={this.state.searchTerm}
-          filterOption={this.state.filterOption}/>
-        <FilterableList
-          files={this.props.files}  
-          searchTerm={this.state.searchTerm}
-          filterOption={this.state.filterOption}/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import SearchBar from './SearchBar';
+// import FilterableList from './FilterableList';
+// import Book from './Book'; 
+// import Header from './Header';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchTerm: '',
+//       filterOption: 'All'
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <SearchBar
+//           searchTerm={this.state.searchTerm}
+//           filterOption={this.state.filterOption}/>
+//         <FilterableList
+//           files={this.props.files}  
+//           searchTerm={this.state.searchTerm}
+//           filterOption={this.state.filterOption}/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Book.js
+++ src/Book.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Book.css';
+import FilterableList from './FilterableList'
 
-https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters
+class Book extends 
+Component {
+    render() {
+     return(
+        <div className="Book">
+        <FilterableList Book={this.props.Book}/>
+        </div> 
+     );      
+}
+}
 
-export default function Book( props ) {
+export default Book;
 
-    const { book } = props;
+// https://www.googleapis.com/apiName/apiVersion/resourcePath?parameters
+
+// export default function Book( props ) {
+
+//     const { book } = props;
         
-    let author;
-    let title;
-    let thumbnailUrl;
-    let previewUrl;
-    let snippet;
-    let cost;
+//     let author;
+//     let title;
+//     let thumbnailUrl;
+//     let previewUrl;
+//     let snippet;
+//     let cost;
 
-    return (
-        <div className="book_container">
-            <a href={ previewUrl } target="blank">
-                <li className="book_li">
-                    <img src={ thumbnailUrl } className="book_image" alt={`The cover of the book titled ${title}`}></img>
-                    <div className="book_info">
-                        <h2 className="book_title">{ title }</h2>
-                        <h4 className="book_author">{ author }</h4>
-                        <div className="book_cost">{ cost }</div>
-                        <p className="book_snippet">{ snippet }</p>
-                    </div>
-                </li>
-            </a>
-        </div>
-    );
-} 
+//     return (
+//         <div className="book_container">
+//             <a href={ previewUrl } target="blank">
+//                 <li className="book_li">
+//                     <img src={ thumbnailUrl } className="book_image" alt={`The cover of the book titled ${title}`}></img>
+//                     <div className="book_info">
+//                         <h2 className="book_title">{ title }</h2>
+//                         <h4 className="book_author">{ author }</h4>
+//                         <div className="book_cost">{ cost }</div>
+//                         <p className="book_snippet">{ snippet }</p>
+//                     </div>
+//                 </li>
+//             </a>
+//         </div>
+//     );
+// } 
