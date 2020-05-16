04759eb38e95b3a77ef0c806d443584661de31ff
AshleyLoreal Leee <53842786+alyrlee@users.noreply.github.com>
2020-05-16 12:52:37 -0400

    Add componentDidMount with fetch to App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SearchBar from './SearchBar';
+import FilterableList from './FilterableList';
+import Book from './Book'; 
+import Header from './Header';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Book: [],
+      showAddForm: false
+    };
   }
-}
 
-export default App;
-
-
-
+ // https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyAMGQrqEdaJOMug4ThmQqLhVTqoseQaLUM
+ //const url = 'https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key={yourAPIKey}';
+ // const yourAPIKey= 'AIzaSyAMGQrqEdaJOMug4ThmQqLhVTqoseQaLUM';
 
 
-// import React, { Component } from 'react';
-// import './App.css';
-// import SearchBar from './SearchBar';
-// import FilterableList from './FilterableList';
-// import Book from './Book'; 
-// import Header from './Header';
+//  const baseUrl = 'https://www.googleapis.com/books/v1/volumes/'
+//     const key = 'AIzaSyAMGQrqEdaJOMug4ThmQqLhVTqoseQaLUM'
+//     const  = this.formatQuery( baseUrl, searchInput, key );
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchTerm: '',
-//       filterOption: 'All'
-//     };
-//   }
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes/';
+    const options = {
+      method: 'GET',
+      headers: {
+      "Authorization": "AIzaSyAMGQrqEdaJOMug4ThmQqLhVTqoseQaLUM",
+      "Content-Type": "application/json"
+      }
+    }; 
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <SearchBar
-//           searchTerm={this.state.searchTerm}
-//           filterOption={this.state.filterOption}/>
-//         <FilterableList
-//           files={this.props.files}  
-//           searchTerm={this.state.searchTerm}
-//           filterOption={this.state.filterOption}/>
-//       </div>
-//     );
-//   }
-// }
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+   .then(res=> res.jscon())
+   .then(data => {
+      this.setState({
+        book: data,
+        error: null
+      });
+   })   
+   .catch(err => {
+      this.setState({
+         error: err.message
+      });
+   });
 
-// export default App;
+  }
+
+  render () {
+    // 
+
+    return (
+        <div className="App">
+          { page }
+        </div>
+      );
+    }
+  }
+
+  export default App; 
